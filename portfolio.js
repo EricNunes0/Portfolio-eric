3077b3d06218c60d8cbab1c2b3dea0abd5878960
Eric Nunes <eric26052004@gmail.com>
2023-05-21 17:27:08 -0300

    Versão 0.0.7 - Menu da versão mobile

--- portfolio.js
+++ portfolio.js
@@ -1,18 +1,24 @@
 const themeButton = document.querySelector('#theme-button');
+const menuThemeButton = document.querySelector('#menu-theme-button');
 const headerSession = document.querySelector('#portfolio-header-header');
 const aboutmeSession = document.querySelector('#portfolio-aboutme-header');
 const projectsSession = document.querySelector('#portfolio-projects-header');
 const contactsSession = document.querySelector('#portfolio-contacts-header');
 
-/* Evento para alterar tema da página */
-themeButton.addEventListener("click", function() {
+/* Função para alterar tema da página */
+function changePageTheme() {
     if(themeButton.className.includes("light")) {
         themeButton.className = "theme-buttons dark";
         headerSession.className = "portfolio-header dark";
         aboutmeSession.className = "portfolio-aboutme dark";
         projectsSession.className = "portfolio-projects dark";
         contactsSession.className = "portfolio-contacts dark";
-        themeButton.innerHTML = "🌙";
+
+        let themeIcons = document.getElementsByClassName("theme-buttons-icons");
+        for(let i = 0; i <= themeIcons.length - 1; i++) {
+            themeIcons[i].src = "https://i.imgur.com/n68qj9b.png"
+        }
+        document.getElementById("about-me").className = "portfolio-aboutme-title dark";
         changeTextsColor(1);
     } else if(themeButton.className.includes("dark")) {
         themeButton.className = "theme-buttons light";
@@ -20,10 +26,15 @@ themeButton.addEventListener("click", function() {
         aboutmeSession.className = "portfolio-aboutme light";
         projectsSession.className = "portfolio-projects light";
         contactsSession.className = "portfolio-contacts light";
-        themeButton.innerHTML = "🌞";
+        
+        let themeIcons = document.getElementsByClassName("theme-buttons-icons");
+        for(let i = 0; i <= themeIcons.length - 1; i++) {
+            themeIcons[i].src = "https://i.imgur.com/EFjsg8D.png"
+        }
+        document.getElementById("about-me").className = "portfolio-aboutme-title light";
         changeTextsColor(0);
     };
-});
+};
 
 /* Alterar a cor de todos os textos */
 function changeTextsColor(theme) {
@@ -43,5 +54,18 @@ function changeTextsColor(theme) {
             console.log(texts[i].className.replace("light-texts", "dark-texts"))
             texts[i].className = texts[i].className.replace("light-texts", "dark-texts");
         }
+    }
+}
+
+function openNavMenu() {
+    let menu = document.getElementById("menu").className
+    if(menu == "nav-menu closed") {
+        document.getElementById("menu").className = "nav-menu opened";
+        document.getElementById("nav-button").className = "open-nav-button opened";
+        document.getElementById("nav-button-image").className = "open-nav-button-image opened";
+    } else {
+        document.getElementById("menu").className = "nav-menu closed";
+        document.getElementById("nav-button").className = "open-nav-button closed";
+        document.getElementById("nav-button-image").className = "open-nav-button-image closed";
     }
 }
